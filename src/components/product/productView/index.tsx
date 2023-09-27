5e4f6e3166ac62bbd8750afb44ac2c840f6ac3a5
maikeg-alves <maicongabrielalves99@gmail.com>
2023-09-27 01:38:02 -0300

    :bug: fix[loja]: product

--- src/components/product/productView/index.tsx
+++ src/components/product/productView/index.tsx
@@ -28,63 +28,73 @@ export const ProductView: React.FC<ProductViewProps> = ({ produto }) => {
               height={350}
               src={
                 currentImage ||
-                produto.attributes?.thumbnail.data.attributes?.url
+                produto?.attributes?.thumbnail?.data?.attributes?.url ||
+                ''
               }
-              alt={'foto:' + produto.attributes.title}
+              alt={'foto:' + (produto?.attributes?.title || '')}
             />
           </Col>
 
           <div className="galery-list-container">
             <ul className="d-flex flex-column align-items-center">
-              {produto.attributes.gallery.data &&
-                produto.attributes.gallery.data.map((image) => (
-                  <li
-                    key={image.id}
-                    onMouseEnter={() =>
-                      handleThumbnailHover(image.attributes.url)
-                    }
-                    onClick={() => handleThumbnailHover(image.attributes.url)}
-                    onMouseLeave={() =>
-                      setCurrentImage(
-                        produto.attributes.thumbnail.data.attributes.url,
-                      )
-                    }
-                    className="galery"
-                  >
-                    <Image
-                      width={60}
-                      height={60}
-                      src={image.attributes.formats.small.url}
-                      alt={'foto:' + image.attributes.name}
-                    />
-                  </li>
-                ))}
+              {(produto?.attributes?.gallery?.data || []).map((image) => (
+                <li
+                  key={image.id}
+                  onMouseEnter={() =>
+                    handleThumbnailHover(image.attributes?.url || '')
+                  }
+                  onClick={() =>
+                    handleThumbnailHover(image.attributes?.url || '')
+                  }
+                  onMouseLeave={() =>
+                    setCurrentImage(
+                      produto?.attributes?.thumbnail?.data?.attributes?.url ||
+                        '',
+                    )
+                  }
+                  className="galery"
+                >
+                  <Image
+                    width={60}
+                    height={60}
+                    src={image.attributes?.formats?.small?.url || ''}
+                    alt={'foto:' + (image.attributes?.name || '')}
+                  />
+                </li>
+              ))}
 
-              {produto.attributes.colors_imgs &&
-                produto.attributes.colors_imgs.map((image) => (
-                  <li
-                    key={image.id}
-                    onMouseEnter={() =>
-                      handleThumbnailHover(image.img_color.data.attributes.url)
+              {(produto?.attributes?.colors_imgs || []).map((image) => (
+                <li
+                  key={image.id}
+                  onMouseEnter={() =>
+                    handleThumbnailHover(
+                      image.img_color?.data?.attributes?.url || '',
+                    )
+                  }
+                  onClick={() =>
+                    handleThumbnailHover(
+                      image.img_color?.data?.attributes?.url || '',
+                    )
+                  }
+                  onMouseLeave={() =>
+                    setCurrentImage(
+                      produto?.attributes?.thumbnail?.data?.attributes?.url ||
+                        '',
+                    )
+                  }
+                  className="galery"
+                >
+                  <Image
+                    width={60}
+                    height={60}
+                    src={
+                      image.img_color?.data?.attributes?.formats?.small?.url ||
+                      ''
                     }
-                    onClick={() =>
-                      handleThumbnailHover(image.img_color.data.attributes.url)
-                    }
-                    onMouseLeave={() =>
-                      setCurrentImage(
-                        produto.attributes.thumbnail.data.attributes.url,
-                      )
-                    }
-                    className="galery"
-                  >
-                    <Image
-                      width={60}
-                      height={60}
-                      src={image.img_color.data.attributes.formats.small.url}
-                      alt={'foto:' + image.color_name}
-                    />
-                  </li>
-                ))}
+                    alt={'foto:' + (image.color_name || '')}
+                  />
+                </li>
+              ))}
             </ul>
           </div>
         </Container>
@@ -95,7 +105,7 @@ export const ProductView: React.FC<ProductViewProps> = ({ produto }) => {
           className="d-flex justify-content-center align-items-center d-lg-none"
         >
           <Carousel data-bs-theme="dark">
-            {produto.attributes.thumbnail.data && (
+            {produto?.attributes?.thumbnail?.data ? (
               <Carousel.Item key={produto.id}>
                 <Image
                   width={350}
@@ -104,38 +114,36 @@ export const ProductView: React.FC<ProductViewProps> = ({ produto }) => {
                   alt={`foto: ${produto.attributes.title}`}
                 />
               </Carousel.Item>
-            )}
+            ) : null}
 
-            {produto.attributes.gallery.data &&
-              produto.attributes.gallery.data.map((image) => (
-                <Carousel.Item key={image.id}>
-                  <Image
-                    width={350}
-                    height={350}
-                    src={image.attributes.url}
-                    alt={`foto: ${image.attributes.name}`}
-                  />
-                </Carousel.Item>
-              ))}
+            {produto?.attributes?.gallery?.data?.map((image) => (
+              <Carousel.Item key={image.id}>
+                <Image
+                  width={350}
+                  height={350}
+                  src={image.attributes.url}
+                  alt={`foto: ${image.attributes.name}`}
+                />
+              </Carousel.Item>
+            ))}
 
-            {produto.attributes.colors_imgs &&
-              produto.attributes.colors_imgs.map((image) => (
-                <Carousel.Item key={image.id}>
-                  <Image
-                    width={350}
-                    height={350}
-                    src={
-                      image.img_color.data.attributes.url ||
-                      'URL_DA_IMAGEM_PADRÃO_AQUI'
-                    }
-                    alt={`foto: ${image.color_name}`}
-                  />
-                </Carousel.Item>
-              ))}
+            {produto?.attributes?.colors_imgs?.map((image) => (
+              <Carousel.Item key={image.id}>
+                <Image
+                  width={350}
+                  height={350}
+                  src={
+                    image.img_color?.data?.attributes?.url ||
+                    'URL_DA_IMAGEM_PADRÃO_AQUI'
+                  }
+                  alt={`foto: ${image.color_name}`}
+                />
+              </Carousel.Item>
+            ))}
 
-            {!produto.attributes.thumbnail.data &&
-              !produto.attributes.gallery.data &&
-              !produto.attributes.colors_imgs && (
+            {!produto?.attributes?.thumbnail?.data &&
+              !produto?.attributes?.gallery?.data &&
+              !produto?.attributes?.colors_imgs && (
                 <Carousel.Item>
                   <p>Nenhuma imagem disponível</p>
                 </Carousel.Item>
